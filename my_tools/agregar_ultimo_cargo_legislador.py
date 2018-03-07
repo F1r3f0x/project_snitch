a3a1b0a1c701a988d6c98c97b46e5a4f85d690cd
plabin <plabin@outlook.cl>
2018-03-07 00:06:02 -0300

    Reescrito agregar_ultimo_cargo_legislador

--- my_tools/agregar_ultimo_cargo_legislador.py
+++ my_tools/agregar_ultimo_cargo_legislador.py
@@ -3,43 +3,21 @@
     Patricio Labin Correa - 2018
     GPLv3
 """
-import pymysql.cursors
-
-DB_HOST = 'example.net'
-DB_HOST = 'example.net'
-DB_PORT = 3306
-DB_USER = 'user'  # Credenciales Aqui!!!
-DB_PASS = 'pass'  # Y Aqui
-DB_NAME = 'snitch'
-DB_CHARSET = 'utf8mb4'
+from project_snitch import db
+from project_snitch.models import Legislador, CargoLegislativo
 
 
 def agregar_ultimo_cargo():
     """
     Genera texto_buscable para legisladores en la base de datos.
     """
-    conn = pymysql.connect(host=DB_HOST,
-                           port=DB_PORT,
-                           user=DB_USER,
-                           db=DB_NAME,
-                           password=DB_PASS,
-                           charset=DB_CHARSET,
-                           cursorclass=pymysql.cursors.DictCursor)
+    legisladores = Legislador.query.all()
 
-    with conn.cursor() as cur:
-        query = 'SELECT * FROM legislador'
-        cur.execute(query)
-        legisladores = cur.fetchall()
+    for legislador in legisladores:
+        ultimo_cargo = CargoLegislativo.query.filter_by(legislador_id=legislador.id).order_by(CargoLegislativo.fecha_ingreso.desc())[0]
+        legislador.ultimo_tipo_legislador_id = ultimo_cargo.id
 
-        query_reemplazo = "UPDATE legislador SET ultimo_tipo_legislador_id=%s where id=%s"
-
-        for legislador in legisladores:
-            cur.execute('SELECT * from snitch.cargo_legislativo WHERE legislador_id = %s', (legislador['id'],))
-            cargo = cur.fetchone()
-
-            cur.execute(query_reemplazo, (cargo['tipo_legislador_id'], legislador['id']))
-
-        conn.commit()
+    db.session.commit()
 
 if __name__ == '__main__':
     agregar_ultimo_cargo()
