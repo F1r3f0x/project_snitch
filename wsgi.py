c0f94d802d213b2f9679383c467171eb616d07ce
F1r3f0x <plabin@outlook.cl>
2018-03-14 08:32:45 -0300

    Remover espacios y print()

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,6 @@
     Punto de Entrada (Debug)
 """
 from project_snitch import app
-from project_snitch import routes, admin_views
 
 if __name__ == "__main__":
     app.run(port=5000, threaded=True, debug=True)
