2f133ab0277fe34b0cbefcc37d3125caad345014
F1r3f0x <plabin@outlook.cl>
2018-03-14 22:50:39 -0300

    Fix rutas

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,7 @@
     Punto de Entrada (Debug)
 """
 from project_snitch import app
+from project_snitch import routes, admin_views
 
 if __name__ == "__main__":
     app.run(port=5000, threaded=True, debug=True)
