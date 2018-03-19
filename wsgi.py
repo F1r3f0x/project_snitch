ade96cce095999ac8896984ee74d18dcc4f2435b
F1r3f0x <plabin@outlook.cl>
2018-03-18 23:17:13 -0300

    Quitar threaded (puede producir bugs) Desplegar con uwsgi o similar..

--- wsgi.py
+++ wsgi.py
@@ -5,4 +5,4 @@ from project_snitch import app
 from project_snitch import routes, admin_views
 
 if __name__ == "__main__":
-    app.run(port=5000, threaded=True, debug=True)
+    app.run(port=5000, debug=True)
