bd5bd74c846d9176bc7ca00730c243034e8e55ba
F1r3f0x <plabin@outlook.cl>
2018-04-16 08:48:03 -0300

    application.py

--- application.py
+++ application.py
@@ -9,4 +9,7 @@ print(sys.path)
 from app import app, routes, admin_views
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+
+    application = app
+
+    application.run(port=5000, debug=True)
